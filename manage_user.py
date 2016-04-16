976b0e1a8b5db8d8c7d540dcc2a02095b1ba7d87
Shang <tig314@gmail.com>
2016-04-15 23:59:38 -0400

    improve argparse..

--- manage_user.py
+++ manage_user.py
@@ -1,7 +1,6 @@
 import sys
 import os
 import argparse
-# This file create & maintain csv files where user emails are stored
 
 region_list = ['dc', 'nyc', 'baltimore', 'philly']
 
@@ -27,16 +26,16 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='add or delete users for a region'
                                                  '(in a csv file), use -h for usage')
     parser.add_argument('email', nargs='+', help='user email to be added')
-    parser.add_argument('--region', nargs=1, required=True, choices=region_list, help='region to be subscribed')
+    parser.add_argument('--region', required=True, choices=region_list, help='region to be subscribed')
     parser.add_argument('-a', dest='action', action='store_const', const='add', help='add')
     parser.add_argument('-d', dest='action', action='store_const', const='del', help='delete user')
     args = parser.parse_args(sys.argv[1:])
     if args.action in 'add':
         print 'add user ' + str(args.email) + ' to ' + str(args.region)
         for each_user in args.email:
-            add_new_user(each_user, args.region[0])
+            add_new_user(each_user, args.region)
     else:
         print 'delete user ' + str(args.email) + ' from ' + str(args.region)
         for each_user in args.email:
-            delete_user(each_user, args.region[0])
+            delete_user(each_user, args.region)
 
